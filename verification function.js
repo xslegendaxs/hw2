c7dc7717049d4b7db1ec2914837500c8f439df31
xslegendaxs <axenti.07@mail.ru>
2024-03-20 20:52:28 +0300

    verification function

---
+++ verification function.js
@@ -0,0 +1,12 @@
+let object = {};                             // чтобы проверить надо написать {key: 1/'привет'}
+console.log(isObjectEmpty(object));
+
+
+function isObjectEmpty(obj) {             //Функция isObjectEmpty принимает объект obj в качестве аргумента.
+    for (let key in obj) {               //Тут в цикле for...in происходит перебор всех ключей объекта.
+        if (obj.hasOwnProperty(key)) {  //hasOwnProperty() содержит ли объект какое либо свойство/значение.
+            return false;              //Если хотя бы одна переменная обнаружена функция сразу возвращает false.
+        }
+    }
+    return true;                     //Если цикл завершается и не было найдено переменных функция возвращает true.
+}
